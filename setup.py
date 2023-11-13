6576ce1e1cbe0a4049efdd5e52058e2023777f1b
Prachurya Nath <prachuryanath00@gmail.com>
2023-11-14 01:43:47 +0530

    Added __init__.py, requirements. and setup file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup 
+  
+setup( 
+    name='math_quiz', 
+    version='0.0.1', 
+    description='A python package having a simple math quiz game', 
+    author='Prachurya Nath', 
+    author_email='prachurya.nath@fau.de', 
+    packages=['math_quiz'], 
+    install_requires=[ 
+        'random'
+    ], 
+)
